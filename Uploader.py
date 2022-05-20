2d0c92638f404cffb2c198697a51cd04783fd63b
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2022-05-20 11:30:15 -0700

    update

--- Uploader.py
+++ Uploader.py
@@ -12,7 +12,7 @@ def main():
         DB_USER = os.getenv("DB_USER")
         DB_USER_PASS = os.getenv("DB_USER_PASS")
         DB_PORT = os.getenv("DB_PORT")
-    
+        print(DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_USER_PASS)
         draftyearpathway = os.listdir(r'D:\Coding Projects\Hockey Stats\DraftYear.csv')        
         for files in draftyearpathway:
             #Connection to postgres database.
@@ -23,6 +23,8 @@ def main():
             #verifies that the pathway is printing the appropriate pathway. Commented out for convenience in the future. 
             #print(r'D:\Coding Projects\Hockey Stats\DraftYear.csv'+ '\\'+files)
             
+            
+
             #creating cursor to interact with the database.
             cur=conn.cursor()
             
@@ -30,18 +32,17 @@ def main():
             print('\ncreated curser object:', cur)
 
             #selects and copies the appropriate .csv file into the Hockey Draft Database
-            cur.copy_from(copypath,'bighockeydata')
+            #cur.copy_from(copypath,'bighockeydata')
             
             #Commits changes
-            conn.commit()
+            #conn.commit()
             
             #Closes the cursor object
             cur.close()
             print('\nclosed cursor object:',cur)
 
-    #have to actually call the function or it won't work. 
     connect_and_store()
-        
+    
 
 
 if __name__ == "__main__":
