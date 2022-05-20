bc5b064e49351c5938a5d70c78ad645aaa9c0f9e
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2022-05-20 12:57:32 -0700

    update jsontypeconverter

---
+++ JsonTypeConverter.py
@@ -0,0 +1,15 @@
+def main():
+    import json
+    import os
+    #function takes in a .json file reads each item 
+    def take_and_convert():
+        directory_list = os.listdir(r'D:\Coding Projects\Hockey Stats\DraftYear.json')
+
+        for files in directory_list:
+            #objective: take in intial file. Read values and convert the data type where necessary.
+
+
+
+            
+if __name__ == '__main__':
+    main()
