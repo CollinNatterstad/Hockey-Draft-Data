d241d7082dbb6353ef2467193cd48c85f710dc6b
CollinNatterstad <collin.ragnarokarchery@gmail.com>
2022-06-16 12:16:08 -0700

    update

--- class_based_solution.py
+++ class_based_solution.py
@@ -1,6 +1,6 @@
+from multiprocessing.connection import Connection
 import os
 import json
-from zlib import DEF_BUF_SIZE
 import psycopg2 as pg
 from psycopg2 import sql
 from dotenv import load_dotenv
@@ -8,7 +8,8 @@ load_dotenv()
 
 ''' 
     the purpose of this program is to create a class based library of discrete solutions for the files that I've already created. This is so that I can better understand 
-    object oriented programming and how class based libraries can facilitate scripting functionality by creating a library of commonly performed tasks. 
+    object oriented programming and how class based libraries can facilitate scripting functionality by creating a library of commonly performed tasks postgreSQL components.
+    Furthermore, this will incorporate many of the  
     
     S(ingle responsibility): classes and methods that perform a single thing with high cohesion
     O(pen/closed): open to function extension but closed to modification (you shouldn't need to change the code to extend the functionality) 
@@ -41,13 +42,20 @@ class connection(Psql_Interaction):
         self.cur = self.conn.cursor()
         return self.conn, self.cur 
     
-    def __exit__(self,):
-        self.conn.close()
+    def __exit__(self,exception_type, exception_value, exception_traceback):
         self.cur.close()
+        self.conn.close()
+
 
     
     #variables are stored in local .env file. stipulates the connection factors for the postgress server 
-c1 = connection(os.getenv("DB_HOST"), os.getenv("DB_NAME"), os.getenv("DB_USER"), os.getenv("DB_USER_PASS"), os.getenv("DB_PORT")) 
+db_host= os.getenv("DB_HOST")
+db_name= os.getenv("DB_NAME")
+db_user= os.getenv("DB_USER")
+db_user_pass= os.getenv("DB_USER_PASS")
+db_port= os.getenv("DB_PORT")
 
-with connection() as conn:
+#calls the connection class and supplies appropriate arguments. With acts as a context manager 
+with connection(db_name, db_user, db_user_pass,db_host,db_port) as conn:
+    self.cur.execute("""select * from player_table limit 1""")
     
